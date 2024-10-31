8f4a3908eae9d5e75dadec56ec79f7337e733154
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2024-10-31 00:55:04 -0400

    done

---
+++ backend/pages/api/auth/signup.js
@@ -0,0 +1,37 @@
+import prisma from "../../../lib/prisma";
+import bcrypt from "bcrypt";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({ error: "Please fill all fields" });
+    }
+
+    console.log(req.body);
+
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({ message: "User created successfully" });
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
