0090798f6738fbf909389f70d98550354cf8327c
vivic04 <117671628+vivic04@users.noreply.github.com>
2024-11-01 20:30:06 -0400

    FIXED comments, voting for both works

---
+++ backend/prisma/migrations/20241101220019_add_blogpost_ratings/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_BlogPost" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "hidden" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "authorId" INTEGER NOT NULL,
+    "upvotes" INTEGER NOT NULL DEFAULT 0,
+    "downvotes" INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT "BlogPost_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_BlogPost" ("authorId", "content", "createdAt", "description", "hidden", "id", "title") SELECT "authorId", "content", "createdAt", "description", "hidden", "id", "title" FROM "BlogPost";
+DROP TABLE "BlogPost";
+ALTER TABLE "new_BlogPost" RENAME TO "BlogPost";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
